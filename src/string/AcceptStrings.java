957d30aa1130f495ab235acc118eb4d05b50981e
MITALI BOSE <73990307+Phoenixinfinity@users.noreply.github.com>
2020-11-23 19:17:09 +0530

    Add files via upload

---
+++ src/string/AcceptStrings.java
@@ -0,0 +1,28 @@
+package string;
+import java.util.Scanner;
+public class AcceptStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj = new Scanner(System.in);
+                System.out.println("\nEnter the string: \n");
+                System.out.println("String 1:");
+                String one = obj.nextLine();
+                System.out.println("String 2:");
+                String two = obj.nextLine();
+                System.out.println("String 3:");
+                String three = obj.nextLine();
+                System.out.println("String 4:");
+                String four = obj.nextLine();
+                System.out.println("String 5:");
+                String five = obj.nextLine();
+                System.out.println("\nThe entered strings are: \n");
+                System.out.println(one);
+                System.out.println(two);
+                System.out.println(three);
+                System.out.println(four);
+                System.out.println(five);
+              
+	}
+
+}
